2fba9747cbba6c8ea4f19f6a9d0e3a5c32408ebb
choiByeongOk <cbo0920@gmail.com>
2021-01-17 22:24:40 +0900

    apply Expanded

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -30,25 +30,52 @@ class _HomeState extends State<Home> {
         child: Icon(Icons.add),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+                width: double.maxFinite,
+                height: 48,
+                decoration: BoxDecoration(
+                    color: const Color(0xffffffff)
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 7,
+                        child:
+                        Container( // search icon
+                          padding: const EdgeInsets.only(left: 20),
+                          alignment: Alignment.centerLeft,
+                          width: 32,
+                          height: 32,
+                          child: Image.asset(
+                              "assets/icons/account_circle_default_24_px.png"),
+                        ),
+                      ),
+                      Expanded(
+                        child:
+                        Container( // search icon
+                          width: 32,
+                          height: 32,
+                          child: Image.asset("assets/icons/search_24_px.png"),
+                        ),
+                      ),
+                      Expanded(
+                        child:
+                        Container( // search icon
+                          padding: const EdgeInsets.only(right: 20),
+                          width: 32,
+                          height: 32,
+                          child: Image.asset("assets/icons/notifications_none_24_px.png"),
+                        ),
+                      ),
+                    ],
+                  )
+                )
+            ),
             RaisedButton(
               child: Text('Go Page A'),
               onPressed: () {
