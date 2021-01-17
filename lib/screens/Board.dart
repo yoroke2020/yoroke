5992d97b85170cfbb211ebfa6ad7c47fafa3f76d
Moongee Cho <35756853+mcdaniel13@users.noreply.github.com>
2021-01-17 20:56:48 +0900

    Create page frame
    Create the page frame of the board page

--- lib/screens/Board.dart
+++ lib/screens/Board.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Board extends StatefulWidget {
   @override
@@ -8,8 +9,119 @@ class Board extends StatefulWidget {
 class _BoardState extends State<Board> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('board page'),
-    );
+    return Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Container(
+                decoration: BoxDecoration(color: const Color(0xffffffff)),
+                child: new AspectRatio(
+                    aspectRatio: 360 / 40,
+                    child: Stack(children: <Widget>[
+                      Positioned(
+                        top: 8,
+                        left: 16,
+                        child: Text("후기",
+                            style: const TextStyle(
+                                color: const Color(0xe6000000),
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "NotoSansCJKkr",
+                                fontStyle: FontStyle.normal,
+                                fontSize: 16.0),
+                            textAlign: TextAlign.left),
+                      ),
+                      Positioned(
+                        top: 8,
+                        right: 16,
+                        child: Container(
+                          width: 24,
+                          height: 24,
+                          child: Image.asset("assets/icons/navigate_next_24_px.png")
+                        ),
+                      ),
+                    ]))),
+            Container(
+                decoration: BoxDecoration(color: const Color(0xffffffff)),
+                child: new AspectRatio(
+                  aspectRatio: 360 / 120,
+                )),
+            Container(
+                decoration: BoxDecoration(color: const Color(0xffffffff)),
+                child: new AspectRatio(
+                    aspectRatio: 360 / 40,
+                    child: Stack(children: <Widget>[
+                      Positioned(
+                        top: 8,
+                        left: 16,
+                        child: Text("고민/질문",
+                            style: const TextStyle(
+                                color: const Color(0xe6000000),
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "NotoSansCJKkr",
+                                fontStyle: FontStyle.normal,
+                                fontSize: 16.0),
+                            textAlign: TextAlign.left),
+                      ),
+                      Positioned(
+                        top: 8,
+                        right: 16,
+                        child: Container(
+                          width: 24,
+                          height: 24,
+                          child: Image.asset("assets/icons/navigate_next_24_px.png")
+                        ),
+                      ),
+                    ]))),
+            Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xffffffff),
+                ),
+                child: new AspectRatio(
+                    aspectRatio: 360 / 296,
+                    child: Column(children: <Widget>[
+                    ]
+                    )
+                )
+            ),
+            Container(
+                decoration: BoxDecoration(color: const Color(0xffffffff)),
+                child: new AspectRatio(
+                    aspectRatio: 360 / 40,
+                    child: Stack(children: <Widget>[
+                      Positioned(
+                        top: 8,
+                        left: 16,
+                        child: Text("구인구직",
+                            style: const TextStyle(
+                                color: const Color(0xe6000000),
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "NotoSansCJKkr",
+                                fontStyle: FontStyle.normal,
+                                fontSize: 16.0),
+                            textAlign: TextAlign.left),
+                      ),
+                      Positioned(
+                        top: 8,
+                        right: 16,
+                        child: Container(
+                            width: 24,
+                            height: 24,
+                            child: Image.asset("assets/icons/navigate_next_24_px.png")
+                        ),
+                      ),
+                    ]))),
+            Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xffffffff),
+                ),
+                child: new AspectRatio(
+                    aspectRatio: 360 / 296,
+                    child: Column(children: <Widget>[
+                    ]
+                    )
+                )
+            ),
+                      ],
+        ));
   }
 }
