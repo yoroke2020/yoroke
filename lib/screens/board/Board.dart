e4b88fb6b1803f850893a4b8a4efcb645edc2abc
Jinho.Choi <ddjddd@naver.com>
2021-04-26 22:48:21 +0900

    Resolve merge Conflicg

--- lib/screens/board/Board.dart
+++ lib/screens/board/Board.dart
@@ -10,16 +10,11 @@ import 'package:yoroke/screens/common/appbars/AppBarNormal.dart';
 
 import 'BoardCardListItem.dart';
 
-class Board extends StatefulWidget {
+class Board extends StatelessWidget {
   Board({required this.onPushNavigator});
 
   final ValueChanged<YrkData>? onPushNavigator;
 
-  @override
-  _BoardState createState() => _BoardState();
-}
-
-class _BoardState extends State<Board> {
   final bardCardListItemCount = 12;
 
   final qnaPageController = PageController();
@@ -32,7 +27,7 @@ class _BoardState extends State<Board> {
         width: 64.0,
         height: 76.0,
         index: i,
-        onPushNavigator: widget.onPushNavigator,
+        onPushNavigator: onPushNavigator,
       ));
     }
     return list;
@@ -42,8 +37,7 @@ class _BoardState extends State<Board> {
     List<Widget> list = <Widget>[];
     for (int i = 0; i < 4; i++) {
       list.add(YrkListView(
-          itemCount: 4,
-          item: _buildList(i, subPageItem, widget.onPushNavigator!)));
+          itemCount: 4, item: _buildList(i, subPageItem, onPushNavigator!)));
     }
     return list;
   }
@@ -66,7 +60,7 @@ class _BoardState extends State<Board> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBarNormal(
-          onPushNavigator: widget.onPushNavigator,
+          onPushNavigator: onPushNavigator,
         ),
         body: ListView(
           children: <Widget>[
---
+++ lib/screens/common/appbars/AppBarNormalArrowBack.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppBarNormalArrowBack extends StatelessWidget
+    implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(48.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.maxFinite,
+        height: 48.0,
+        margin: EdgeInsets.symmetric(horizontal: 16.0),
+        decoration: BoxDecoration(color: const Color(0xffffffff)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+              child: Padding(
+                padding: EdgeInsets.zero,
+                child: Image.asset(
+                  "assets/icons/icon_arrow_back_24_px.png",
+                  width: 24.0,
+                  height: 24.0,
+                ),
+              ),
+              onTap: () => Navigator.pop(context),
+            ),
+            Spacer(),
+            InkWell(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Image.asset(
+                    "assets/icons/search_24_px.png",
+                    width: 24.0,
+                    height: 24.0,
+                  ),
+                ),
+                onTap: () => print("AppBarNormalArrowBack search pressed")),
+            InkWell(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Image.asset(
+                    "assets/icons/create_24_px.png",
+                    width: 24.0,
+                    height: 24.0,
+                  ),
+                ),
+                onTap: () => print("AppBarNormalArrowBack create pressed")),
+            InkWell(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 4.0),
+                  child: Image.asset(
+                    "assets/icons/notifications_none_24_px.png",
+                    width: 24.0,
+                    height: 24.0,
+                  ),
+                ),
+                onTap: () =>
+                    print("AppBarNormalArrowBack notification pressed")),
+          ],
+        ));
+  }
+}
