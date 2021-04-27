3d8c6d5ce7687e0a9ff3f4884c83a33b6274913b
Moongee Cho <whansrl92@gmail.com>
2021-04-28 01:40:35 +0900

    Reformat BottomBarComment

--- lib/screens/common/bottombars/BottomBarComment.dart
+++ lib/screens/common/bottombars/BottomBarComment.dart
@@ -15,16 +15,10 @@ class _BottomBarCommentState extends State<BottomBarComment> {
 
   @override
   Widget build(BuildContext context) {
-    print(MediaQuery
-        .of(context)
-        .size
-        .height);
+    print(MediaQuery.of(context).size.height);
 
     return Transform.translate(
-        offset: Offset(0.0, -1 * MediaQuery
-            .of(context)
-            .viewInsets
-            .bottom),
+        offset: Offset(0.0, -1 * MediaQuery.of(context).viewInsets.bottom),
         child: BottomAppBar(
           child: Container(
             padding: EdgeInsets.symmetric(horizontal: 8.0),
@@ -32,56 +26,55 @@ class _BottomBarCommentState extends State<BottomBarComment> {
             height: 56.0,
             decoration: BoxDecoration(color: const Color(0xfffffffff)),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-            Expanded(
-            child: YrkTextField(
-              label: "댓글을 남겨보세요",
-              textFieldType: TextFieldType.comment,
-            ),
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: YrkTextField(
+                      label: "댓글을 남겨보세요",
+                      textFieldType: TextFieldType.comment,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () => setState(() {
+                      isPrivate = isPrivate ? false : true;
+                    }),
+                    child: Container(
+                      width: 32.0,
+                      height: 32.0,
+                      margin: EdgeInsets.symmetric(horizontal: 8.0),
+                      decoration: BoxDecoration(
+                          color: const Color(0xffffffff),
+                          borderRadius: BorderRadius.all(Radius.circular(32)),
+                          border: Border.all(
+                            color: const Color(0x4d000000),
+                            width: 1.0,
+                          )),
+                      child: Center(
+                        child: isPrivate
+                            ? Image.asset("assets/icons/icon_lock_24_px.png",
+                                color: const Color(0x4d000000),
+                                width: 20.0,
+                                height: 20.0)
+                            : Image.asset(
+                                "assets/icons/icon_lock_open_24_px.png",
+                                color: const Color(0x4d000000),
+                                width: 20.0,
+                                height: 20.0),
+                      ),
+                    ),
+                  ),
+                  YrkButton(
+                    buttonType: ButtonType.outlinechip,
+                    label: '등록',
+                    btnColor: const Color(0x4d000000),
+                    fontColor: const Color(0x4d000000),
+                    onPressed: () => print("register button clicked"),
+                    width: 64.0,
+                    height: 32.0,
+                  ),
+                ]),
           ),
-          InkWell(
-            onTap: () =>
-                setState(() {
-                  isPrivate = isPrivate ? false : true;
-                }),
-            child: Container(
-            width: 32.0,
-            height: 32.0,
-            margin: EdgeInsets.symmetric(horizontal: 8.0),
-            decoration: BoxDecoration(
-                color: const Color(0xffffffff),
-                borderRadius: BorderRadius.all(Radius.circular(32)),
-                border: Border.all(
-                  color: const Color(0x4d000000),
-                  width: 1,
-                )),
-            child: Center(
-              child: isPrivate
-                  ? Image.asset("assets/icons/icon_lock_24_px.png",
-                  color: const Color(0x4d000000),
-                  width: 20.0,
-                  height: 20.0)
-                  : Image.asset("assets/icons/icon_lock_open_24_px.png",
-                  color: const Color(0x4d000000),
-                  width: 20.0,
-                  height: 20.0),
-            ),
-          ),
-        ),
-        YrkButton(
-          buttonType: ButtonType.outlinechip,
-          label: '등록',
-          btnColor: const Color(0x4d000000),
-          fontColor: const Color(0x4d000000),
-          onPressed: () => print("register button clicked"),
-          width: 64,
-          height: 32,
-        ),
-        ]),)
-    ,
-    )
-    );
+        ));
   }
 }
