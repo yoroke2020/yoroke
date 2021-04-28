81f698e88ee4253f45fe3caf39c081f3ab5211e9
Jinho.Choi <ddjddd@naver.com>
2021-04-28 17:42:05 +0900

    Add EditProfile page

--- lib/screens/common/YrkDrawer.dart
+++ lib/screens/common/YrkDrawer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:yoroke/models/YrkData.dart';
 import 'package:yoroke/screens/common/YrkTextStyle.dart';
+import 'package:yoroke/screens/mypage/EditProfile.dart';
 
 var tiles = [
   {"text": "프로필 편집", "icon": "assets/icons/icon_naver.png"},
@@ -24,72 +26,81 @@ var imagelist = [
   "assets/icons/icon_apple.png",
 ];
 
-ListView getList() {
-  return ListView.separated(
-      itemCount: textlist.length + 1,
-      separatorBuilder: (_, __) => const Divider(
-            color: Color(0xffe5e5e5),
-            thickness: 1,
-            indent: 60,
-            height: 1,
-          ),
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          // return the header
-          return DrawerHeader(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: InkWell(
-                        onTap: () {},
-                        child: Container(
-                          padding: EdgeInsets.zero,
-                          width: 44,
-                          height: 44,
-                          child: Image.asset(
-                              "assets/icons/account_circle_default_24_px.png"),
-                        )),
-                  ),
-                  Text(
-                    "비회원",
-                    style: YrkTextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            decoration: BoxDecoration(
-                color: Colors.white, border: Border.all(color: Colors.white)),
-          );
-        }
-        index -= 1;
-        // final data = groceryListNames[index];
-        return ListTile(
-          leading: Container(
-            padding: EdgeInsets.zero,
-            child: Image.asset(
-              imagelist[index],
-              width: 22,
-              height: 22,
-            ),
-          ),
-          title: Container(
-            padding: EdgeInsets.zero,
-            child: Text(
-              // tiles[index]['text']!,
-              textlist[index],
-              style: YrkTextStyle(fontSize: 16, height: 1),
-              textAlign: TextAlign.left,
-            ),
-          ),
-        );
-      });
-}
-
 class YrkDrawer extends Drawer {
-  YrkDrawer({Key? key}) : super(key: key, child: getList());
+  final ValueChanged<YrkData>? onPushNavigator;
+  YrkDrawer({
+    Key? key,
+    this.onPushNavigator,
+  }) : super(
+            key: key,
+            child: ListView.separated(
+                itemCount: textlist.length + 1,
+                separatorBuilder: (_, __) => const Divider(
+                      color: Color(0xffe5e5e5),
+                      thickness: 1,
+                      indent: 60,
+                      height: 1,
+                    ),
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    // return the header
+                    return DrawerHeader(
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Center(
+                              child: InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => EditProfile()),
+                                    );
+                                  },
+                                  child: Container(
+                                    padding: EdgeInsets.zero,
+                                    width: 44,
+                                    height: 44,
+                                    child: Image.asset(
+                                        "assets/icons/account_circle_default_24_px.png"),
+                                  )),
+                            ),
+                            Text(
+                              "비회원",
+                              style: YrkTextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.white)),
+                    );
+                  }
+                  index -= 1;
+                  // final data = groceryListNames[index];
+                  return ListTile(
+                    leading: Container(
+                      padding: EdgeInsets.zero,
+                      child: Image.asset(
+                        imagelist[index],
+                        width: 22,
+                        height: 22,
+                      ),
+                    ),
+                    title: Container(
+                      padding: EdgeInsets.zero,
+                      child: Text(
+                        // tiles[index]['text']!,
+                        textlist[index],
+                        style: YrkTextStyle(fontSize: 16, height: 1),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  );
+                }));
 }
