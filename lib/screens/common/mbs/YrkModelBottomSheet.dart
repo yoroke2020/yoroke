c9b4d800481792abb62d8e9be1a790300a50c2d3
Moongee Cho <whansrl92@gmail.com>
2021-04-28 19:14:10 +0900

    Implement modalBottomSheet with RadioButton & Image

--- lib/screens/common/mbs/YrkModelBottomSheet.dart
+++ lib/screens/common/mbs/YrkModelBottomSheet.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'YrkListView.dart';
+import 'YrkMbsImageList.dart';
+import 'YrkMbsRadioButtonList.dart';
+
+enum YrkModelBottomSheetType {
+  post,
+  createPost,
+}
 
 class YrkModelBottomSheet extends StatelessWidget {
   const YrkModelBottomSheet(
-      {required this.listItem, this.listHeight = 120, this.listItemCount = 0});
+      {required this.type,
+      this.title = "Default",
+      required this.labelList,
+      this.imageList,
+      this.listHeight = 120,
+      this.onTap});
 
-  final List<Widget> listItem;
-  final int listItemCount;
+  final YrkModelBottomSheetType type;
+  final String? title;
+  final List<String> labelList;
+  final List<String>? imageList;
   final double listHeight;
+  final ValueChanged<int>? onTap;
+
+  Widget getModalWidget(BuildContext context) {
+    switch (type) {
+      case YrkModelBottomSheetType.post:
+        return YrkMbsImageList(labelList: labelList, imageList: imageList!, onTap: onTap!);
+      case YrkModelBottomSheetType.createPost:
+        return YrkMbsRadioButtonList(title: title!, labelList: labelList, onTap: onTap!);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +43,9 @@ class YrkModelBottomSheet extends StatelessWidget {
         child: new Container(
             decoration: new BoxDecoration(
                 color: const Color(0xffffffff),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(16.0), topRight: Radius.circular(16.0)
-                    )),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0))),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -36,11 +60,7 @@ class YrkModelBottomSheet extends StatelessWidget {
                           icon: Image.asset("assets/icons/icon_clear_24_px.png",
                               width: 14.0, height: 14.0),
                         ))),
-                YrkListView(
-                  item: listItem,
-                  height: listHeight,
-                  itemCount: listItemCount,
-                ),
+                getModalWidget(context)
               ],
             )));
   }
