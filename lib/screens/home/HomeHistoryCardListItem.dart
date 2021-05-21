bf99a7fae44358e9301c6335988658203b8112ca
Moongee Cho <whansrl92@gmail.com>
2021-05-21 23:44:10 +0900

    Remove YrkListView at Home

--- lib/screens/home/HomeHistoryCardListItem.dart
+++ lib/screens/home/HomeHistoryCardListItem.dart
@@ -33,141 +33,144 @@ class HomeHistoryCardListItem extends StatelessWidget {
               width: width,
               height: height,
               margin: const EdgeInsets.only(bottom: 2),
-              child: Row(
-                children: [
-                  Expanded(
-                      flex: 120,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
-                          image: new DecorationImage(
-                            image:
-                                new AssetImage(testCardImage.elementAt(index)),
-                            fit: BoxFit.fill,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 120,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                            image: new DecorationImage(
+                              image:
+                                  new AssetImage(testCardImage.elementAt(index)),
+                              fit: BoxFit.fill,
+                            ),
                           ),
-                        ),
-                      )),
-                  Expanded(
-                    flex: 224,
-                    child: Column(
-                      children: <Widget>[
-                        Expanded(
-                            flex: 27,
-                            child: Container(
-                              margin:
-                                  const EdgeInsets.only(left: 16, bottom: 4),
-                              alignment: Alignment.bottomLeft,
-                              child: Text("조문기네 요양원",
-                                  style: const TextStyle(
-                                      color: const Color(0xe6000000),
-                                      fontWeight: FontWeight.w700,
-                                      fontFamily: "NotoSansCJKKR",
-                                      fontStyle: FontStyle.normal,
-                                      fontSize: 18.0),
-                                  textAlign: TextAlign.left),
-                            )),
-                        Expanded(
-                            flex: 20,
-                            child: Container(
-                              margin: const EdgeInsets.only(left: 16),
-                              alignment: Alignment.bottomLeft,
-                              child: Text("서울시 마포구",
-                                  style: const TextStyle(
-                                      color: const Color(0x99000000),
-                                      fontWeight: FontWeight.w500,
-                                      fontFamily: "NotoSansCJKKR",
-                                      fontStyle: FontStyle.normal,
-                                      fontSize: 14.0),
-                                  textAlign: TextAlign.left),
-                            )),
-                        Expanded(
-                          flex: 3,
-                          child: Container(),
-                        ),
-                        Expanded(
-                            flex: 17,
-                            child: Container(
+                        )),
+                    Expanded(
+                      flex: 224,
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                              flex: 27,
+                              child: Container(
+                                margin:
+                                    const EdgeInsets.only(left: 16, bottom: 4),
+                                alignment: Alignment.bottomLeft,
+                                child: Text("조문기네 요양원",
+                                    style: const TextStyle(
+                                        color: const Color(0xe6000000),
+                                        fontWeight: FontWeight.w700,
+                                        fontFamily: "NotoSansCJKKR",
+                                        fontStyle: FontStyle.normal,
+                                        fontSize: 18.0),
+                                    textAlign: TextAlign.left),
+                              )),
+                          Expanded(
+                              flex: 20,
+                              child: Container(
                                 margin: const EdgeInsets.only(left: 16),
-                                child: Row(children: <Widget>[
-                                  YrkStarRating(
-                                      rating: 4.8,
-                                      eachWidth: 12,
-                                      eachHeight: 11),
-                                  Container(
-                                    margin: const EdgeInsets.only(left: 6),
-                                    alignment: Alignment.bottomLeft,
-                                    child: Text("4.8 (12)",
-                                        style: const TextStyle(
-                                            color: const Color(0x99000000),
-                                            fontWeight: FontWeight.w700,
-                                            fontFamily: "OpenSans",
-                                            fontStyle: FontStyle.normal,
-                                            fontSize: 12.0),
-                                        textAlign: TextAlign.left),
-                                  )
-                                ]))),
-                        Expanded(
-                          flex: 4,
-                          child: Container(),
-                        ),
-                        Expanded(
-                            flex: 18,
-                            child: Container(
-                              margin: const EdgeInsets.only(left: 16),
-                              alignment: Alignment.bottomLeft,
-                              child: Text("시설등급",
-                                  style: const TextStyle(
-                                      color: const Color(0x99000000),
-                                      fontWeight: FontWeight.w400,
-                                      fontFamily: "NotoSansCJKKR",
-                                      fontStyle: FontStyle.normal,
-                                      fontSize: 12.0),
-                                  textAlign: TextAlign.left),
-                            )),
-                        Expanded(
-                          flex: 18,
-                          child: Container(),
-                        ),
-                        Expanded(
-                            flex: 27,
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  Container(
-                                      margin: const EdgeInsets.only(right: 4),
-                                      child: Text("월 평균",
+                                alignment: Alignment.bottomLeft,
+                                child: Text("서울시 마포구",
+                                    style: const TextStyle(
+                                        color: const Color(0x99000000),
+                                        fontWeight: FontWeight.w500,
+                                        fontFamily: "NotoSansCJKKR",
+                                        fontStyle: FontStyle.normal,
+                                        fontSize: 14.0),
+                                    textAlign: TextAlign.left),
+                              )),
+                          Expanded(
+                            flex: 3,
+                            child: Container(),
+                          ),
+                          Expanded(
+                              flex: 17,
+                              child: Container(
+                                  margin: const EdgeInsets.only(left: 16),
+                                  child: Row(children: <Widget>[
+                                    YrkStarRating(
+                                        rating: 4.8,
+                                        eachWidth: 12,
+                                        eachHeight: 11),
+                                    Container(
+                                      margin: const EdgeInsets.only(left: 6),
+                                      alignment: Alignment.bottomLeft,
+                                      child: Text("4.8 (12)",
                                           style: const TextStyle(
-                                              color: const Color(0xe6000000),
-                                              fontWeight: FontWeight.w400,
-                                              fontFamily: "NotoSansCJKKR",
-                                              fontStyle: FontStyle.normal,
-                                              fontSize: 12.0),
-                                          textAlign: TextAlign.left)),
-                                  Container(
-                                      margin: const EdgeInsets.only(right: 4),
-                                      child: Text("722,390",
-                                          style: const TextStyle(
-                                              color: const Color(0xe6000000),
+                                              color: const Color(0x99000000),
                                               fontWeight: FontWeight.w700,
                                               fontFamily: "OpenSans",
                                               fontStyle: FontStyle.normal,
-                                              fontSize: 20.0),
-                                          textAlign: TextAlign.left)),
-                                  Container(
-                                      margin: const EdgeInsets.only(right: 16),
-                                      child: Text("원",
-                                          style: const TextStyle(
-                                              color: const Color(0xe6000000),
-                                              fontWeight: FontWeight.w400,
-                                              fontFamily: "NotoSansCJKKR",
-                                              fontStyle: FontStyle.normal,
                                               fontSize: 12.0),
-                                          textAlign: TextAlign.left)),
-                                ])),
-                      ],
-                    ),
-                  )
-                ],
+                                          textAlign: TextAlign.left),
+                                    )
+                                  ]))),
+                          Expanded(
+                            flex: 4,
+                            child: Container(),
+                          ),
+                          Expanded(
+                              flex: 18,
+                              child: Container(
+                                margin: const EdgeInsets.only(left: 16),
+                                alignment: Alignment.bottomLeft,
+                                child: Text("시설등급",
+                                    style: const TextStyle(
+                                        color: const Color(0x99000000),
+                                        fontWeight: FontWeight.w400,
+                                        fontFamily: "NotoSansCJKKR",
+                                        fontStyle: FontStyle.normal,
+                                        fontSize: 12.0),
+                                    textAlign: TextAlign.left),
+                              )),
+                          Expanded(
+                            flex: 18,
+                            child: Container(),
+                          ),
+                          Expanded(
+                              flex: 27,
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    Container(
+                                        margin: const EdgeInsets.only(right: 4),
+                                        child: Text("월 평균",
+                                            style: const TextStyle(
+                                                color: const Color(0xe6000000),
+                                                fontWeight: FontWeight.w400,
+                                                fontFamily: "NotoSansCJKKR",
+                                                fontStyle: FontStyle.normal,
+                                                fontSize: 12.0),
+                                            textAlign: TextAlign.left)),
+                                    Container(
+                                        margin: const EdgeInsets.only(right: 4),
+                                        child: Text("722,390",
+                                            style: const TextStyle(
+                                                color: const Color(0xe6000000),
+                                                fontWeight: FontWeight.w700,
+                                                fontFamily: "OpenSans",
+                                                fontStyle: FontStyle.normal,
+                                                fontSize: 20.0),
+                                            textAlign: TextAlign.left)),
+                                    Container(
+                                        margin: const EdgeInsets.only(right: 16),
+                                        child: Text("원",
+                                            style: const TextStyle(
+                                                color: const Color(0xe6000000),
+                                                fontWeight: FontWeight.w400,
+                                                fontFamily: "NotoSansCJKKR",
+                                                fontStyle: FontStyle.normal,
+                                                fontSize: 12.0),
+                                            textAlign: TextAlign.left)),
+                                  ])),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             )));
   }
