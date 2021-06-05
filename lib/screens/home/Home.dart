aa04de6f80649408e0336b66ce28b34eb9457834
choiByeongOk <cbo0920@gmail.com>
2021-06-05 17:03:02 +0900

    Add pull to refresh on Home

--- lib/screens/home/Home.dart
+++ lib/screens/home/Home.dart
@@ -147,105 +147,117 @@ class _HomeState extends State<Home> with ScreenState<HomeBlock> {
   Widget build(BuildContext context) {
     PopularPostBlock popularPostBlock =
         block.findFirstBlockWhere("PopularPostBlock") as PopularPostBlock;
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-            onPressed: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => TestPage()))),
-        appBar: YrkAppBar(
-          type: YrkAppBarType.accountCircleAll,
-          curPageItem: RootPageItem.home,
-        ),
-        drawer: yrkDrawer,
-        bottomNavigationBar: BottomBarNavigation.getInstance(RootPageItem.home),
-        body: ListView(children: <Widget>[
-          Container(
-              height: 120.0,
-              alignment: Alignment.centerLeft,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                    return HomeCardListItem(
-                        width: 320.0, height: 120.0, index: index);
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return SizedBox(width: 8.0);
-                  },
-                  itemCount: 4)),
-          YrkTabHeaderView(
-              title: popularPostBlock.title,
-              titleStyle: YrkTextStyle(
-                  color: const Color(0xe6000000),
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "NotoSansCJKKR",
-                  fontStyle: FontStyle.normal,
-                  fontSize: 16.0),
-              clickable: true,
-              nextSubPageItem: "boardQna",
-              customIcon: Row(
-                children: [
-                  YrkIconButton(
-                    icon: "icon_create.svg",
-                  ),
-                  TextButton(
-                    child: Text("글 작성",
-                        style: const TextStyle(
-                            color: const Color(0x99000000),
-                            fontWeight: FontWeight.w500,
-                            fontFamily: "NotoSansCJKKR",
-                            fontStyle: FontStyle.normal,
-                            fontSize: 14.0),
-                        textAlign: TextAlign.left),
-                    onPressed: null,
-                  )
-                ],
-              )),
-          // YrkPage(
-          //   page: _yrkListView(SubPageItem.post),
-          //   controller: popularPageController,
-          //   isIndicatorEnabled: true,
-          // ),
-          YrkPage(
-            page: _buildPopularPost("post", popularPostBlock),
-            controller: popularPageController,
-            isIndicatorEnabled: true,
-          ),
-          YrkTabHeaderView(
-              title: "인기 의료시설",
-              titleStyle: YrkTextStyle(
-                  color: const Color(0xe6000000),
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "NotoSansCJKKR",
-                  fontStyle: FontStyle.normal,
-                  fontSize: 16.0),
-              clickable: true,
-              nextSubPageItem: "boardQna",
-              customIcon: TextButton(
-                  child: Text("전체보기",
-                      style: const TextStyle(
-                          color: const Color(0x99000000),
-                          fontWeight: FontWeight.w500,
-                          fontFamily: "NotoSansCJKKR",
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14.0),
-                      textAlign: TextAlign.left),
-                  onPressed: () => _onHomeHistoryClicked(context))),
-          Container(
-              height: 200.0,
-              alignment: Alignment.centerLeft,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                    return HomePopularCardListItem(
-                      width: 144.0,
-                      height: 106.0,
-                      index: index,
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return SizedBox(width: 8.0);
-                  },
-                  itemCount: 4)),
-        ]));
+    return RefreshIndicator(
+        onRefresh: () {
+          return Future.delayed(
+            Duration(seconds: 1),
+            () {
+              setState(() {
+                updateBlockOn("pullToRefresh");
+              });
+            },
+          );
+        },
+        child: Scaffold(
+            floatingActionButton: FloatingActionButton(
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TestPage()))),
+            appBar: YrkAppBar(
+              type: YrkAppBarType.accountCircleAll,
+              curPageItem: RootPageItem.home,
+            ),
+            drawer: yrkDrawer,
+            bottomNavigationBar:
+                BottomBarNavigation.getInstance(RootPageItem.home),
+            body: ListView(children: <Widget>[
+              Container(
+                  height: 120.0,
+                  alignment: Alignment.centerLeft,
+                  child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return HomeCardListItem(
+                            width: 320.0, height: 120.0, index: index);
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(width: 8.0);
+                      },
+                      itemCount: 4)),
+              YrkTabHeaderView(
+                  title: popularPostBlock.title,
+                  titleStyle: YrkTextStyle(
+                      color: const Color(0xe6000000),
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "NotoSansCJKKR",
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16.0),
+                  clickable: true,
+                  nextSubPageItem: "boardQna",
+                  customIcon: Row(
+                    children: [
+                      YrkIconButton(
+                        icon: "icon_create.svg",
+                      ),
+                      TextButton(
+                        child: Text("글 작성",
+                            style: const TextStyle(
+                                color: const Color(0x99000000),
+                                fontWeight: FontWeight.w500,
+                                fontFamily: "NotoSansCJKKR",
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14.0),
+                            textAlign: TextAlign.left),
+                        onPressed: null,
+                      )
+                    ],
+                  )),
+              // YrkPage(
+              //   page: _yrkListView(SubPageItem.post),
+              //   controller: popularPageController,
+              //   isIndicatorEnabled: true,
+              // ),
+              YrkPage(
+                page: _buildPopularPost("post", popularPostBlock),
+                controller: popularPageController,
+                isIndicatorEnabled: true,
+              ),
+              YrkTabHeaderView(
+                  title: "인기 의료시설",
+                  titleStyle: YrkTextStyle(
+                      color: const Color(0xe6000000),
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "NotoSansCJKKR",
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16.0),
+                  clickable: true,
+                  nextSubPageItem: "boardQna",
+                  customIcon: TextButton(
+                      child: Text("전체보기",
+                          style: const TextStyle(
+                              color: const Color(0x99000000),
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "NotoSansCJKKR",
+                              fontStyle: FontStyle.normal,
+                              fontSize: 14.0),
+                          textAlign: TextAlign.left),
+                      onPressed: () => _onHomeHistoryClicked(context))),
+              Container(
+                  height: 200.0,
+                  alignment: Alignment.centerLeft,
+                  child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return HomePopularCardListItem(
+                          width: 144.0,
+                          height: 106.0,
+                          index: index,
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(width: 8.0);
+                      },
+                      itemCount: 4)),
+            ])));
   }
 
   void _onHomeHistoryClicked(BuildContext context) async {
@@ -285,7 +297,7 @@ class _HomeState extends State<Home> with ScreenState<HomeBlock> {
   @override
   void updateBlockOn(String action) {
     switch (action) {
-      case "tabTouched":
+      case "pullToRefresh":
         PopularPostBlock postBlock =
             block.findFirstBlockWhere("PopularPostBlock") as PopularPostBlock;
         block.blocks!.remove(postBlock);
@@ -300,12 +312,5 @@ class _HomeState extends State<Home> with ScreenState<HomeBlock> {
       default:
         break;
     }
-  }
-
-  // sample code
-  void _onTabTouched() {
-    setState(() {
-      updateBlockOn("tabTouched");
-    });
   }
 }
